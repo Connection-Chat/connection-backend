a99860555283dba1c4f2502873edabaf876123aa
Mykola-Maik <mykoluamaik@gmail.com>
2025-01-23 18:26:49 +0200

    quick fix

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -7,7 +7,7 @@ export const generateToken = (userId, res) => {
 
   res.cookie("token", token, {
     httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-    sameSite: "lax", // CSRF attacks cross-site request forgery attacks
+    sameSite: "none", // CSRF attacks cross-site request forgery attacks
     secure: process.env.NODE_ENV !== "development", // http or https
     maxAge: 1000 * 60 * 60 * 24 * 7, // MS
   });
